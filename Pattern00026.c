66fa18e79b1c4a7d3815060ccd23dbfe26a4ccf5
Sarif Iqbal <iqbalsarif.official@gmail.com>
2022-08-23 00:24:43 +0530

    Create Pattern00026.c

---
+++ Pattern00026.c
@@ -0,0 +1,26 @@
+/*
+
+11111
+2222
+333
+44
+5
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= 5; j++)
+        {
+            if (j<=6-i)
+                printf("%d", i);
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
