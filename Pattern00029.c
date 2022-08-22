d532d45a92e1f2e3f36283c6faa56fb2c7e2a8e4
Sarif Iqbal <iqbalsarif.official@gmail.com>
2022-08-23 00:50:44 +0530

    Create Pattern00029.c

---
+++ Pattern00029.c
@@ -0,0 +1,26 @@
+/*
+
+  *   *   *
+ * * * * *
+*   *   *
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 3; i++)
+    {
+        for (j = 1; j <= 11; j++)
+        {
+            if (((i + j) % 4 == 0) || (i == 2) && (j % 4 == 0))
+            {
+                printf("*");
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+}
