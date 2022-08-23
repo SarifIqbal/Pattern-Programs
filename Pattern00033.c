df8306992f3487ab8fb6a572eab13e41214ff5b3
Sarif Iqbal <iqbalsarif.official@gmail.com>
2022-08-23 10:02:44 +0530

    Create Pattern00033.c

---
+++ Pattern00033.c
@@ -0,0 +1,28 @@
+/*
+
+   *****
+  *****
+ *****
+*****
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    char k;
+    for (i = 1; i <= 4; i++)
+    {
+        k = 'A';
+        for (j = 1; j <= 8; j++)
+        {
+            if (j >= 5 - i && j <= 9 - i)
+                printf("*");
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
