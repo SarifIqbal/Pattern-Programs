c6a12cd27be303304adea5322f05230de7b4e981
Sarif Iqbal <iqbalsarif.official@gmail.com>
2022-08-23 10:03:14 +0530

    Create Pattern00034.c

---
+++ Pattern00034.c
@@ -0,0 +1,33 @@
+/*
+
+   1
+  12A
+ 123AB
+1234ABC
+
+*/
+
+#include <stdio.h>
+int main()
+{
+    int i, j;
+    char k;
+    for (i = 1; i <= 4; i++)
+    {
+        k = 49;
+        for (j = 1; j <= 7; j++)
+        {
+            if (j >= 5 - i && j <= 3 + i)
+            {
+                printf("%c", k);
+                k++;
+                if (j == 4)
+                    k = 65;
+            }
+            else
+                printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
